c7a1e7da6141e4086b43f813377569a491600ee3
RK-rgb <ramasree650@gmail.com>
2026-06-04 17:11:44 +0530

    Update check.py

--- check.py
+++ check.py
@@ -6,8 +6,14 @@ BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
 PRODUCTS = {
-    "Maroon": "https://www.hmtwatches.store/product/77733243-645c-4eac-8e69-63425e1cc09b",
-    "Pink": "https://www.hmtwatches.store/product/e1b512c1-35d7-4941-bc1d-c7e3f016de15"
+    "Quartz Maroon-Kohinoor": "https://www.hmtwatches.store/product/77733243-645c-4eac-8e69-63425e1cc09b",
+    "Quartz Pink-Kohinoor": "https://www.hmtwatches.store/product/e1b512c1-35d7-4941-bc1d-c7e3f016de15",
+    "Automatic Blue-Kohinoor": "https://www.hmtwatches.store/product/dc26b232-4231-4b58-bed5-984282684852",
+    "Automatic Maroon-Kohinoor": "https://www.hmtwatches.store/product/79ec4cbd-d085-48a9-9127-7c0d583d45d6",
+    "Automatic Yellow-Kohinoor": "https://www.hmtwatches.store/product/34aef933-9cb9-4a12-bbe3-7041e1c90677",
+    "Automatic Yellow-Pink": "https://www.hmtwatches.store/product/fcd3bd02-1ee3-4ed4-8381-ca2cd2258139",
+    "HMT Tareeq Quartz-Tiffany Blue": "https://www.hmtwatches.store/product/7281c42e-604a-4bd9-b011-066aa202eddd",
+    
 }
 
 def send_telegram(msg):
